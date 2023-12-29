1c6c71d84286ef9ee2d77ca2c3238fb0e5bf1a28
BouaziziMahdi <mahdibouazizi10@gmail.com>
2023-12-29 12:53:15 +0100

    "aad models "

---
+++ src/test/java/com/bridge/bridge/BridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridge.bridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
